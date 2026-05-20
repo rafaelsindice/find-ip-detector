cce31eddec2d66ccee5b38b06c8d749367e9b5d7
rafael Magno <rafaelmagnum90@hotmail.com>
2026-05-20 11:43:00 -0300

    comit inicial

---
+++ src/main/java/com/br/rafaelsindice/ipdiscovery/findipdetector/FindIpDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.br.rafaelsindice.ipdiscovery.findipdetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindIpDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindIpDetectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/rafaelsindice/ipdiscovery/find_ip_detector/FindIpDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.rafaelsindice.ipdiscovery.find_ip_detector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindIpDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
